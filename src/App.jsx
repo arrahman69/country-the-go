67c562093130e6e93a7cd5c26bfcb0c370676d45
Abdur Rahman <tohaali332@gmail.com>
2025-09-19 09:28:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Country from './components/Country';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1> hello world</h1>
+<Country></Country>
+    </div>
+  );
+};
+
+export default App;
